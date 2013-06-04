1191ce538315e9e87c46c4429335d2d58f0c1dcf
Yehor Nazarkin <nimnull@gmail.com>
2013-06-04 10:29:12 +0300

    Added custom endpoints for processing addition/edition

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 
 from django.conf.urls import patterns, url
-from .views import PeopleView, DashboardView
+from .views import PeopleView, DashboardView, PeopleEdit
 
 urlpatterns = patterns('core.views',
     url(r'^dashboard/$', DashboardView.as_view(), name='dashboard'),
     url(r'^people/$', PeopleView.as_view(), name='people'),
+    url(r'^people/add/$', PeopleEdit.as_view(), name='people_add'),
+    url(r'^people/edit/(?P<id>\d+)$', PeopleEdit.as_view(), name='people_edit'),
 )
