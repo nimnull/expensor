7322eceb556ccf2843bb176e14cb3fc65feb052c
Yehor Nazarkin <nimnull@gmail.com>
2013-07-31 01:14:52 +0300

    Added account form

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,16 @@
 # encoding: utf-8
 from __future__ import absolute_import
 from django import forms
-from .models import Person
+from .models import Person, Account
 
 
 class PersonForm(forms.ModelForm):
 
     class Meta:
         model = Person
+
+
+class AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        exclude = ('amount',)
